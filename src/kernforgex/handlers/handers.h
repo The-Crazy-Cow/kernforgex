e888b7a6b720d2441b07d1ccc76973b5b5a97d20
alice <jdj17180@gmail.com>
2026-09-01 13:31:00 +0200

    handlers - implement handlers routine canevars

---
+++ src/kernforgex/handlers/handers.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0
+//
+// vim: set ts=8 sw=8 noet tw=80 cc=80 fo+=t :
+
+#ifndef KERNFORGEX_HANDLERS_H
+#define KERNFORGEX_HANDLERS_H
+
+int kfgx_handler_init();
+
+#endif /*KERNFORGEX_HANDLERS_H*/
